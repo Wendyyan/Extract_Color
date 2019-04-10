09139ba13d7511c02b1e82f4e49d2287a384ce57
zhengyf <zhengyf@chinaoly.com>
2019-04-10 16:43:57 +0800

    bug修改

--- app/src/main/java/com/zyf/extractcolor/MainActivity.java
+++ app/src/main/java/com/zyf/extractcolor/MainActivity.java
@@ -1,5 +1,6 @@
 package com.zyf.extractcolor;
 
+import android.annotation.SuppressLint;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
@@ -7,6 +8,7 @@ import android.graphics.drawable.BitmapDrawable;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.graphics.Palette;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
@@ -24,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView tvColor;
     private Bitmap bitmap;
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,13 +43,15 @@ public class MainActivity extends AppCompatActivity {
                 int y = (int) event.getY();
                 //根据坐标获取
                 Bitmap bitmap = ((BitmapDrawable)ivFloor.getDrawable()).getBitmap();
-                int pixel = bitmap.getPixel(x,y);//获取颜色
-                int redValue = Color.red(pixel);
-                int greenValue = Color.green(pixel);
-                int blueValue = Color.blue(pixel);
-                tvColor.setText("颜色：" + redValue + ", " + greenValue + ", " + blueValue);
-                ivColor.setBackgroundColor(Color.rgb(redValue, greenValue, blueValue));
-                return false;
+                if (x < bitmap.getWidth() && y < bitmap.getHeight()) {
+                    int pixel = bitmap.getPixel(x,y);//获取颜色
+                    int redValue = Color.red(pixel);
+                    int greenValue = Color.green(pixel);
+                    int blueValue = Color.blue(pixel);
+                    tvColor.setText("颜色：" + redValue + ", " + greenValue + ", " + blueValue);
+                    ivColor.setBackgroundColor(Color.rgb(redValue, greenValue, blueValue));
+                }
+                return true;
             }
         });
 
@@ -54,12 +59,24 @@ public class MainActivity extends AppCompatActivity {
         new Thread(new Runnable() {
             @Override
             public void run() {
-                bitmap = returnBitmap("http://122.226.100.102:8003/upload/www/1/navigation/201709251627244590.png");
+                bitmap = returnBitmap("https://hbimg.huabanimg.com/efea83a54ffd6bb2135cfe33451ff5b368c82b5a138232-wK6giD_fw658");
 
                 handler.post(new Runnable() {
                     @Override
                     public void run() {
                         ivFloor.setImageBitmap(bitmap);
+                        Palette.from(bitmap).generate(new Palette.PaletteAsyncListener() {
+                            @Override
+                            public void onGenerated(Palette palette) {
+                                Palette.Swatch vibrant = palette.getVibrantSwatch();
+                                if (vibrant != null) {
+                                    tvColor.setTextColor(vibrant.getTitleTextColor());
+                                    tvColor.setText("" + vibrant.getRgb());
+                                    ivColor.setBackgroundColor(vibrant.getRgb());
+                                }
+
+                            }
+                        });
                     }
                 });
             }
